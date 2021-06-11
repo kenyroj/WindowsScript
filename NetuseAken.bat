bafd1db58c9ab609ab3b1180f7a873393da8e2ab
aken.hsu (許斐勝 - MDT) <aken.hsu@mic.com.tw>
2021-06-11 11:08:14 +0800

    Update netuse script for AKenHsu

--- NetuseAken.bat
+++ NetuseAken.bat
@@ -2,11 +2,15 @@
 @SET DEV02_SERVER=MDT-APBC-RD5-DEV02.mic.com.tw
 @SET DEV03_SERVER=MDT-APBC-RD5-DEV03.mic.com.tw
 @SET GERRIT_SERVER=MDT-GERRIT01.mic.com.tw
+@SET JENKINS_SERVER=MDT-JENKINS01.mic.com.tw
 @SET FILE_SERVER=MDT-APBC-RD5-FILE01.mic.com.tw
 @SET TEST_SERVER=MDT-APBC-RD5-TEST01.mic.com.tw
 @SET LOCAL_VM=192.168.56.105
 @SET USERNAME=aken.hsu
 @SET PASSWORD=1q2w3e4r
+@SET MDTNAME=mdtuser
+@SET MDTPASSWD=Aa122333
+
 
 :: Setting for the Development server
 NET USE \\%DEV01_SERVER%\ /DELETE
@@ -23,15 +27,27 @@ NET USE V: /delete
 NET USE V: \\%DEV02_SERVER%\%USERNAME% /user:%USERNAME% %PASSWORD% /persistent:no
 
 
+:: Setting for the Jenkins server
+NET USE \\%JENKINS_SERVER%\ /DELETE
+
+NET USE L: /delete
+NET USE L: \\%JENKINS_SERVER%\ReleaseImageRoot /user:%USERNAME% %PASSWORD% /persistent:no
+
+NET USE O: /delete
+NET USE O: \\%JENKINS_SERVER%\%USERNAME% /user:%USERNAME% %PASSWORD% /persistent:no
+
+
 :: Setting for the Gerrit server
 NET USE \\%GERRIT_SERVER%\ /DELETE
 
-NET USE L: /delete
-NET USE L: \\%GERRIT_SERVER%\ReleaseImage /user:%USERNAME% %PASSWORD% /persistent:no
+::NET USE P: /delete
+::NET USE P: \\%GERRIT_SERVER%\%USERNAME% /user:%USERNAME% %PASSWORD% /persistent:no
 
-NET USE O: /delete
-NET USE O: \\%GERRIT_SERVER%\%USERNAME% /user:%USERNAME% %PASSWORD% /persistent:no
+NET USE Q: /delete
+NET USE Q: \\%GERRIT_SERVER%\ReleaseImage /user:%MDTNAME% %MDTPASSWD% /persistent:no
 
+NET USE R: /delete
+NET USE R: \\%GERRIT_SERVER%\%MDTNAME% /user:%MDTNAME% %MDTPASSWD% /persistent:no
 
 :: Setting for the File server
 NET USE \\%FILE_SERVER%\ /DELETE
@@ -50,10 +66,7 @@ NET USE S: \\%FILE_SERVER%\Share /user:%USERNAME% %PASSWORD% /persistent:no
 NET USE \\%TEST_SERVER%\ /DELETE
 
 NET USE T: /delete
-NET USE T: \\%TEST_SERVER%\%USERNAME% /user:%USERNAME% %PASSWORD% /persistent:no
+NET USE T: \\%TEST_SERVER%\%MDTNAME% /user:%MDTNAME% %MDTPASSWD% /persistent:no
 
-
-::NET USE Z: /delete
-::NET USE Z: "\\%LOCAL_VM%\KENyroj Home" /user:%USERNAME% %PASSWORD% /persistent:no
 
 PAUSE
