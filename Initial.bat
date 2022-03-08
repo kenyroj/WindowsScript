568518b4267d53c18b5bf0862eb7a0845bc7df11
aken.hsu (許斐勝 - MDT) <aken.hsu@mic.com.tw>
2022-03-08 09:38:51 +0800

    Add some adb alias

--- Initial.bat
+++ Initial.bat
@@ -1,24 +1,25 @@
 @echo off
 
 rem Doskey Alias
-@doskey ls=ls --color -F
+@doskey ls=ls --color -F $*
+@doskey grep=grep --color=always $*
 @doskey DevMgr=start devmgmt.msc
 @doskey SndCtl=control.exe mmsys.cpl
-@doskey np=D:\NonSetup\NotePad++\notepad++.exe $*
+@doskey np=E:\NonSetup\NotePad++\notepad++.exe $*
 @doskey ..=cd ..
 
 @doskey adev=adb devices
 @doskey aks=adb kill-server
-@doskey arbt=adb reboot
-@doskey aldr=adb reboot bootloader
-@doskey aedl=adb reboot edl
-@doskey aroot=adb root
-@doskey ashll=adb shell
-@doskey acrsh=adb shell "echo c > /proc/sysrq-trigger"
-@doskey ameta=adb shell cat /firmware/verinfo/ver_info.txt
+@doskey arbt=adb wait-for-device reboot
+@doskey aldr=adb wait-for-device reboot bootloader
+@doskey aedl=adb wait-for-device reboot edl
+@doskey aroot=adb wait-for-device root
+@doskey ashll=adb wait-for-device shell $*
+@doskey aver=adb wait-for-device shell "getprop ro.build.display.id"
+@doskey acrsh=adb wait-for-device shell "echo c > /proc/sysrq-trigger"
 @doskey fb_rbt=fastboot reboot
 
-SET PATH=%PATH%;D:\WindowsScript
+SET PATH=%PATH%;E:\WindowsScript;E:\NonSetup\AndroidPlatformTools;E:\NonSetup\GnuWin32\bin
 
 ::PROMPT $E[m$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S
 SET PROMPT_PATH=$E[1;36m$P$E[m
@@ -28,3 +29,5 @@ if "%ConEmuIsAdmin%" == "ADMIN" (
 	SET PROMPT_TAIL=$E[1;33m$G$E[m$S$E]9;12$E\
 )
 PROMPT %PROMPT_PATH%%PROMPT_TAIL%
+
+E:
