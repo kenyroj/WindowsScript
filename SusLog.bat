407a929f8899e91e1c4f4a12f7b87f214021b1cf
aken.hsu (許斐勝 - MDT) <aken.hsu@mic.com.tw>
2022-03-08 14:46:42 +0800

    Update suslog script

--- SusLog.bat
+++ SusLog.bat
@@ -6,5 +6,6 @@ SET GREP_OPT=%GREP_OPT% -e irq_wakeup
 SET GREP_OPT=%GREP_OPT% -e gic_show_resume_irq
 SET GREP_OPT=%GREP_OPT% -e "suspend entry"
 SET GREP_OPT=%GREP_OPT% -e "Suspending console(s)"
+SET GREP_OPT=%GREP_OPT% -e "SusLog"
 
 grep %GREP_OPT% %1 | grep -v deep | grep -v "TedC"
